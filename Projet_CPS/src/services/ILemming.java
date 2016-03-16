3ae5fd51f18730710e78fc8104cbf5aeaac2d55e
Jordi <j.debalanda@gmail.com>
2016-03-16 23:51:14 +0100

    javafied lemming

--- Projet_CPS/src/services/ILemming.java
+++ Projet_CPS/src/services/ILemming.java
@@ -2,20 +2,57 @@ package services;
 
 public interface ILemming {
 	
-	// OBSERVATORS
-	int getX();
-	int getY();
-	int getNumber();
-	Direction getDir();
-	Status getStatus();
-	int timeFalling();
-	IGameEng gameEngine();
+    // OBSERVATORS
+    int getX();
+    int getY();
+    int getNumber();
+    Direction getDir();
+    Status getStatus();
+    int timeFalling();
+    IGameEng gameEngine();
 	
-	// CONSTRUCTORS
-	void init(IGameEng gE);
+    // CONSTRUCTORS
+    void init(IGameEng gE);
 	
-	// OPERATORS
-	void changeDir();
-	void setStatus(Status s);
-	void step();
+    // OPERATORS
+    
+    /**
+     * POST:
+     *  if (getDir()@pre == Droite) getDir() == Gauche
+     *  if (getDir()@pre == Gauche) getDir() == Droite
+     */
+    void changeDir();
+
+    /**
+     * POST:
+     *  getStatus() == s
+     */
+    void setStatus(Status s);
+    /**
+     * POST:
+     *   if (Status == Marcheur)
+     *      if (gameEngine().level().nature(getX()@Pre, getY()@Pre + 1) == VIDE) 
+     *          getStatus() == Tombeur; getX() == getX()@pre; gety() == getY()@pre
+     *      else if (getDir()@pre == Droite)
+     *          if (gameEngine().level().nature(getX()@pre + 1, getY()@pre) != VIDE &&
+     *              gameEngine().level().nature(getX()@pre + 1, getY()@pre - 1) != VIDE)
+     *             getDir() == Gauche; getX() == getX()@pre; gety() == getY()@pre;
+     *          else
+     *             getX() == getX()@pre + 1; gety() == getY()@pre; 
+     *      else
+     *          if (gameEngine().level().nature(getX()@pre - 1, getY()@pre) != VIDE &&
+     *              gameEngine().level().nature(getX()@pre - 1, getY()@pre - 1) != VIDE)
+     *             getDir() == Gauche; getX() == getX()@pre; gety() == getY()@pre
+     *          else
+     *             getX() == getX()@pre - 1; getY() == getY()@pre; 
+     *   else if (Status == Tombeur)
+     *      if (gameEngine().level().nature(getX()@pre, getY()@pre + 1) != VIDE) 
+     *          if (timeFalling() < 8) 
+     *             Status = Marcheur; getX() == getX()@pre; gety() == getY()@pre;
+     *          else
+     *             gameEngine().colony()@pre.size() == gameEngine().colony().size() - 1
+     *      else
+     *          getX() == getX()@pre; getY() == getY()@pre + 1;
+     */
+    void step();
 }
