4882f65bff01ddc2b5b64a42084f8958bb1ee34d
Cédric Ribeiro <kasane.ted@gmail.com>
2016-04-28 20:39:17 +0200

    Modification to add the bomber

--- Projet_CPS/src/decorateur/LemmingDecorateur.java
+++ Projet_CPS/src/decorateur/LemmingDecorateur.java
@@ -13,63 +13,68 @@ public class LemmingDecorateur implements ILemming{
 		this.delegate=delegates;
 	}
 
-	@Override
+	
 	public int getX() {
 		return delegate.getX();
 	}
 
-	@Override
+	
 	public int getY() {
 		return delegate.getY();
 	}
 
-	@Override
+	
 	public int getNumber() {
 		return delegate.getNumber();
 	}
 
-	@Override
+	
 	public Direction getDir() {
 		return delegate.getDir();
 	}
 
-	@Override
+	
 	public Status getStatus() {
 		return delegate.getStatus();
 	}
 
-	@Override
+	
 	public int timeFalling() {
 		return delegate.timeFalling();
 	}
 
-	@Override
+	
 	public IGameEng gameEngine() {
 		return delegate.gameEngine();
 	}
 
-	@Override
+	
 	public void init(IGameEng gE) {
 		delegate.init(gE);
 		
 	}
 
-	@Override
+	
 	public void changeDir() {
 		delegate.changeDir();
 		
 	}
 
-	@Override
+	
 	public void setStatus(Status s) {
 		delegate.setStatus(s);
 		
 	}
 
-	@Override
+	
 	public void step() {
 		delegate.step();
 		
+	}
+
+
+	public void setBomber() {
+		delegate.setBomber();
 	}
 
 }
