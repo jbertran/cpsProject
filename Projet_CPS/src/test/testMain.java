494d01fbacf09473c2aaff4d7fc9cfc746a7b1ae
Cédric Ribeiro <kasane.ted@gmail.com>
2016-04-28 20:37:00 +0200

    new Tests

--- Projet_CPS/src/test/testMain.java
+++ Projet_CPS/src/test/testMain.java
@@ -6,24 +6,53 @@ import implem.Level;
 import services.IGameEng;
 import services.IJoueur;
 import services.ILevel;
+import services.Nature;
+import services.Status;
 
 public class testMain {
 
 	
 	public static void main (String [] args){
+		int SPEED = 1300;
+		int HEIGHT = 10;
+		int WIDTH = 30;
+		
+		int SPAWNSPEED = 4;
+		int SIZECOLONY = 8;
+		
+		int ENTREE_X = 1;
+		int ENTREE_Y = 1;
+		
+		int SORTIE_X=25;
+		int SORTIE_Y=3;
+		
+		int cpt=0;
 		IJoueur joueur=new Joueur();
 		ILevel level=new Level();
 		IGameEng gameEng=new GameEngine();
 		
 		joueur.init(gameEng);
-		level.init(5, 30);
-		gameEng.init(level, 8, 2);
-		level.goPlay(3, 3, 25, 3);
+		level.init(HEIGHT, WIDTH);
+		for(int i=0;i<HEIGHT;i++){
+			for(int j=0;j<WIDTH;j++){
+				if(i>3 || ((j < 22) && (j > 10)))
+				level.setNature(j, i, Nature.DIRT);
+			}
+		}
+		gameEng.init(level, SIZECOLONY, SPAWNSPEED);
+		level.goPlay(ENTREE_X, ENTREE_Y, SORTIE_X, SORTIE_Y);
 		while(!gameEng.gameOver()) {
+			/******** STEP *****/
 			System.out.println(gameEng);
 			gameEng.step();
+			cpt++;
+			if(cpt==3)
+				joueur.spendToken(0, Status.BOMB);
+			if(cpt==6)
+				joueur.spendToken(1, Status.BOMB);
+			/******************/
 			try {
-				Thread.sleep(10);
+				Thread.sleep(SPEED);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
