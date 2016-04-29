58de0f360c30cc5dfa97c6b7632fa0e145efe150
Cédric Ribeiro <kasane.ted@gmail.com>
2016-04-29 17:50:25 +0200

    Update JoueurDecorateur.java

--- Projet_CPS/src/decorateur/JoueurDecorateur.java
+++ Projet_CPS/src/decorateur/JoueurDecorateur.java
@@ -11,29 +11,31 @@ public class JoueurDecorateur implements IJoueur{
 		this.delegate=delegates;
 	}
 
-	@Override
+	
 	public int nbTokens(Status s) {
 		return delegate.nbTokens(s);
 	}
 
-	@Override
+	
 	public void init(IGameEng gE) {
 		delegate.init(gE);
 	}
 
-	@Override
+	
 	public void spendToken(int lemm, Status s) {
 		delegate.spendToken(lemm, s);
 	}
 
-	@Override
+	
 	public void reset() {
 		delegate.reset();	
 	}
 
-	@Override
+	
 	public IGameEng gameEngine() {
 		return delegate.gameEngine();
 	}
-
+	public String toString(){
+		return delegate.toString();
+	}
 }
