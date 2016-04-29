f36c5ffa2d112bfe5f6ba9cfd257ff62b677457a
Cédric Ribeiro <kasane.ted@gmail.com>
2016-04-29 17:48:15 +0200

    Update LemmingContrat.java

--- Projet_CPS/src/contrat/LemmingContrat.java
+++ Projet_CPS/src/contrat/LemmingContrat.java
@@ -6,6 +6,7 @@ import services.ILemming;
 import services.Nature;
 import services.Status;
 import decorateur.LemmingDecorateur;
+import error.PostConditionError;
 
 public class LemmingContrat extends LemmingDecorateur{
 
@@ -17,7 +18,7 @@ public class LemmingContrat extends LemmingDecorateur{
 		if (!(getX() >= 0 && getY() >= 0))
 			throw new Error("Lemming: erreur de position");
 		if (!(getNumber() >= 0 && getNumber() < gameEngine().sizeColony()))
-			throw new Error("Lemming: erreur de numéro");
+			throw new Error("Lemming: erreur de numÃ©ro");
 	}
 
 	public int getX() {
@@ -43,7 +44,11 @@ public class LemmingContrat extends LemmingDecorateur{
 		checkInvariants();
 		return super.getDir();
 	}
-
+	public boolean isBomber(){
+		// Invariants
+		checkInvariants();
+		return super.isBomber();
+	}
 	public Status getStatus() {
 		// Invariants
 		checkInvariants();
@@ -61,7 +66,7 @@ public class LemmingContrat extends LemmingDecorateur{
 		checkInvariants();
 		return super.gameEngine();
 	}
-	
+
 	public void init(IGameEng gE) {
 		int nextlpre = gE.peekNextLemNo();
 		// Invariants
@@ -105,6 +110,11 @@ public class LemmingContrat extends LemmingDecorateur{
 		ILemming [] colonypre = gameEngine().colony().clone();
 		Direction dirpre = getDir();
 		int xpre = getX(), ypre = getY();
+		int timeEx_pre = -1;
+		Status Stat_pre=this.getStatus();
+		if(isBomber()){
+			timeEx_pre=timeExploding();
+		}
 		// Invariants
 		checkInvariants();
 		super.step();
@@ -135,7 +145,7 @@ public class LemmingContrat extends LemmingDecorateur{
 							throw new Error("Lemming: step error");
 				}
 			}
-			else if (getStatus() == Status.FALL)
+			else if (getStatus() == Status.FALL){
 				if (gameEngine().level().nature(xpre, ypre + 1) != Nature.EMPTY) { 
 					if (timeFalling() < 8) {
 						if (!(getStatus() == Status.WALK && getX() == xpre && getY() == ypre))
@@ -148,8 +158,101 @@ public class LemmingContrat extends LemmingDecorateur{
 				else
 					if (!(getX() == xpre && getY() == ypre + 1))
 						throw new Error("Lemming: step error - position after falling");
+			}else if(getStatus() == Status.BUILD){
+
+			}else if(getStatus() == Status.FLOAT){
+
+			}else if(getStatus() == Status.STOP){
+				if(gameEngine().level().nature(this.getX(), this.getY())!=Nature.DIRT 
+						&& gameEngine().level().nature(this.getX(), this.getY()-1)!=Nature.DIRT)
+					throw new PostConditionError("Error, STOPPER  didn't generate DIRT");	
+				if(gameEngine().getLemm(getNumber())!=null)
+					throw new PostConditionError("Error, STOPPER didn't die");
+			}else if(getStatus() == Status.BASH){
+				if(this.getDir()==Direction.DROITE){
+					if(this.gameEngine().level().nature(xpre+1, ypre)==Nature.METAL ||
+							this.gameEngine().level().nature(xpre+1, ypre-1)==Nature.METAL
+							|| this.gameEngine().level().nature(xpre+1, ypre-2)==Nature.METAL){
+						if(this.getStatus()!=Status.WALK)
+							throw new PostConditionError("Error, Basher didn't stop with Metal wall in front");
+					}
+					if(gameEngine().level().nature(xpre+1, ypre)!=Nature.EMPTY ||
+							gameEngine().level().nature(xpre+1, ypre-1)!=Nature.EMPTY ||
+									gameEngine().level().nature(xpre+1, ypre-2)!=Nature.EMPTY)
+						throw new PostConditionError("Error, Basher didn't destroy wall in front");
+
+					if(xpre!=this.getX()-1)
+						throw new PostConditionError("Error, Basher didn't move");
+				}else{
+					if(this.gameEngine().level().nature(xpre-1, ypre)==Nature.METAL ||
+							this.gameEngine().level().nature(xpre-1, ypre-1)==Nature.METAL
+							|| this.gameEngine().level().nature(xpre-1, ypre-2)==Nature.METAL){
+						if(this.getStatus()!=Status.WALK)
+							throw new PostConditionError("Error, Basher didn't stop with Metal wall in front");
+					}
+					if(gameEngine().level().nature(xpre-1, ypre)!=Nature.EMPTY ||
+							gameEngine().level().nature(xpre-1, ypre-1)!=Nature.EMPTY ||
+									gameEngine().level().nature(xpre-1, ypre-2)!=Nature.EMPTY)
+						throw new PostConditionError("Error, Basher didn't destroy wall in front");
+
+					if(xpre!=this.getX()+1)
+						throw new PostConditionError("Error, Basher didn't move");
+				}
+
+			}else if(getStatus() == Status.MINER){
+				if(!isMiningDown())
+					if(gameEngine().level().nature(xpre, ypre+1)!=Nature.METAL){
+						if(gameEngine().level().nature(xpre, ypre+1)!=Nature.EMPTY)
+							throw new PostConditionError("Error, MINER didn't Mine down");
+						if(ypre+2<=15)
+							if(gameEngine().level().nature(xpre, ypre+2)!=Nature.METAL){
+								if(gameEngine().level().nature(xpre, ypre+2)!=Nature.EMPTY)
+									throw new PostConditionError("Error, MINER didn't Mine down");
+							}
+					}else{
+						if(this.getDir()==Direction.DROITE){
+							if(gameEngine().level().nature(xpre+1, ypre)!=Nature.METAL){
+								if(gameEngine().level().nature(xpre+1, ypre)!=Nature.EMPTY)
+									throw new PostConditionError("Error, MINER didn't Mine right");
+								if(gameEngine().level().nature(xpre+1, ypre-1)!=Nature.METAL){
+									if(gameEngine().level().nature(xpre+1, ypre-1)!=Nature.EMPTY)
+										throw new PostConditionError("Error, MINER didn't Mine right");
+								}
+							}else{
+								if(gameEngine().level().nature(xpre-1, ypre)!=Nature.METAL){
+									if(gameEngine().level().nature(xpre-1, ypre)!=Nature.EMPTY)
+										throw new PostConditionError("Error, MINER didn't Mine left");
+									if(gameEngine().level().nature(xpre-1, ypre-1)!=Nature.METAL){
+										if(gameEngine().level().nature(xpre-1, ypre-1)!=Nature.EMPTY)
+											throw new PostConditionError("Error, MINER didn't Mine left");
+									}
+								}
+							}
+						}
+					}
+			}
+
+			if(isBomber()){
+				if(timeExploding()<=timeEx_pre)
+					throw new PostConditionError("Counter of explosion of the bomber didn't go up");
+				if(timeExploding()>4){
+					for(int i=this.getX()-2;i<(this.getX()-2)+5;i++){
+						for(int j=this.getY()-1;j<(this.getY()-1)+3;j++){
+							if(i>=0 && i<=15 && j>=0 && j<=15)
+								if(gameEngine().level().nature(i, j)!=Nature.EMPTY 
+								&& gameEngine().level().nature(i, j)!=Nature.METAL)
+									throw new PostConditionError("Error, Bomber exploding didn't explode DIRT");
+						}
+					}
+					if(gameEngine().getLemm(getNumber())!=null)
+						throw new PostConditionError("Error, Bomber didn't die after explosion");
+				}
+			}
+			// Invariants
+			checkInvariants();
 		}
-		// Invariants
-		checkInvariants();
+	}
+	public String toString(){
+		return super.toString();
 	}
 }
