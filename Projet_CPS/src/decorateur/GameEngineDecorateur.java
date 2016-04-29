750626ed9a2b7dea01f8893f5ef20c2f2d1a8244
Cédric Ribeiro <kasane.ted@gmail.com>
2016-04-29 17:50:03 +0200

    Update GameEngineDecorateur.java

--- Projet_CPS/src/decorateur/GameEngineDecorateur.java
+++ Projet_CPS/src/decorateur/GameEngineDecorateur.java
@@ -11,110 +11,111 @@ public class GameEngineDecorateur implements IGameEng{
 		this.delegate=delegates;
 	}
 
-	@Override
 	public ILemming[] colony() {
 		return delegate.colony();
 	}
 
-	@Override
 	public ILemming getLemm(int ln) {
 		return delegate.getLemm(ln);
 	}
 
-	@Override
 	public int sizeColony() {
 		return delegate.sizeColony();
 	}
 
-	@Override
+
 	public int spawned() {
 		return delegate.spawned();
 	}
 
-	@Override
+	
 	public int spawnSpeed() {
 		return delegate.spawnSpeed();
 	}
 
-	@Override
+	
 	public ILevel level() {
 		return delegate.level();
 	}
 
-	@Override
+	
 	public ILevel levelInit() {
 		return delegate.levelInit();
 	}
 
-	@Override
+	
 	public int tours() {
 		return delegate.tours();
 	}
 
-	@Override
+	
 	public int nbSauves() {
 		return delegate.nbSauves();
 	}
 
-	@Override
+	
 	public int score() {
 		return delegate.score();
 	}
 
-	@Override
+	
 	public boolean obstacle(int x, int y) {
 		return obstacle(x, y);
 	}
 
-	@Override
+	
 	public boolean gameOver() {
 		return delegate.gameOver();
 	}
 
-	@Override
+	
 	public void init(ILevel lvl, int sc, int ss) {
 		delegate.init(lvl, sc, ss);
 		
 	}
 
-	@Override
+	
 	public void addLemming(ILemming l) {
 		delegate.addLemming(l);
 		
 	}
 
-	@Override
+	
 	public void killLemming(int ln) {
 		delegate.killLemming(ln);
 		
 	}
 
-	@Override
+	
 	public void saveLemming(int ln) {
 		delegate.saveLemming(ln);
 		
 	}
 
-	@Override
+	
 	public void step() {
 		delegate.step();
 		
 	}
 
-	@Override
+	
 	public void loadLevel(ILevel lvl, int sc, int ss) {
 		delegate.loadLevel(lvl, sc, ss);
 		
 	}
 
-	@Override
+	
 	public int nextLemNo() {
 		return delegate.nextLemNo();
 	}
 
-	@Override
+	
 	public int peekNextLemNo() {
 		return delegate.peekNextLemNo();
+	}
+	
+	public String toString(){
+		return delegate.toString();
 	}
 
 }
